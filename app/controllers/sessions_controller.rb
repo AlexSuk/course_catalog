76ded9b68144cc2118218bbde7be26f003908185
Alexander Suk <suk.alexander@gmail.com>
2016-03-22 04:44:17 -0400

    changes to sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
   	user = User.find_by(name: params[:name])
     if user and user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to searches_url
+      redirect_to user_path(id: session[:user_id])
     else
       redirect_to login_url, alert: "Invalid user/password combination" 
 
